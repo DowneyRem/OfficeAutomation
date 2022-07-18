5d3a6d0ca5f08dd198944f83b8ce21adfad9a0a6
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2022-07-18 16:40:48 +0800

    Excel 3 New
    批量打印 Excel 文件

--- Excel3/Print.py
+++ Excel3/Print.py
@@ -82,29 +82,48 @@ def findFile(path, *extnames):
 @timethis
 @openFileCheck
 def openWord(path):
+	global word
 	word = Dispatch('Word.Application')  # 非独立进程
 	word.Visible = 1  # 0为后台运行
 	word.DisplayAlerts = 0  # 不显示，不警告
-	docx = word.Documents.Open(path)   # 打开文档
+	docx = word.Documents.Open(path)	# 打开文档
 	# print("打开Word……")
-	docx.Application.Run("打印")   # 运行宏
-	# word.PrintOut(Copies:=1)	  # 未验证
+	docx.Application.Run("打印")  # 运行宏
 	# print("打印中……")
 	docx.Close(True)
 	# time.sleep(1)
-	# word.Quit(1)
 	# word.Quit()
 
 
+@timethis
 @openFileCheck
 def openExcel(path):  # 打开软件手动操作
-	excel = DispatchEx('Excel.Application')  # 独立进程
+	global excel
+	excel = Dispatch('Excel.Application')  # 独立进程
 	excel.Visible = 1  # 0为后台运行
 	excel.DisplayAlerts = 0  # 不显示，不警告
 	xlsx = excel.Workbooks.Open(path)  # 打开文档
-	print("打开Excel……")
-
-
+	# print("打开Excel……")
+	
+	# 设置打印格式
+	xlsx.Application.PrintCommunication = False
+	xlsx.Sheets(1).PageSetup.LeftMargin = excel.InchesToPoints(0.55)
+	xlsx.Sheets(1).PageSetup.RightMargin = excel.InchesToPoints(0.35)
+	xlsx.Sheets(1).PageSetup.TopMargin = excel.InchesToPoints(0.75)
+	xlsx.Sheets(1).PageSetup.BottomMargin = excel.InchesToPoints(0.75)
+	xlsx.Sheets(1).PageSetup.HeaderMargin = excel.InchesToPoints(0.4)
+	xlsx.Sheets(1).PageSetup.FooterMargin = excel.InchesToPoints(0.3)
+	xlsx.Sheets(1).PageSetup.Zoom = False
+	xlsx.Sheets(1).PageSetup.FitToPagesWide = 1 
+	xlsx.Sheets(1).PageSetup.FitToPagesTall = 1
+	xlsx.Application.PrintCommunication = True
+	
+	xlsx.WorkSheets("Sheet1").PrintOut(Copies=1, Collate=True, IgnorePrintAreas=False)
+	# xlsx.Application.Run("PERSONAL.XLSB!打印")  # 运行宏
+	# print("打印中……")
+	xlsx.Close(True)
+	
+	
 def desktop():
 	if "Windows" in platform():  # 其他平台我也没用过
 		key = winreg.OpenKey(winreg.HKEY_CURRENT_USER,
@@ -114,10 +133,12 @@ def desktop():
 	
 @timethis	
 def main():
-	path1, path2 = "", ""
-	path = os.path.join(desktop(),"path1","path2")
-	pathlist = findFile(path, ".doc", ".docx")
+	path = os.path.join(desktop(),"name1","name2")
+	pathlist = findFile(path, ".doc", ".docx",".xls", ".xlsx")
+	# pathlist = pathlist[:-2]
 	pathlist.reverse()
+	print(len(pathlist))
+	time.sleep(3)
 	for i in range(len(pathlist)):
 		file = pathlist[i]
 		name = os.path.basename(file)
@@ -126,15 +147,28 @@ def main():
 		if len(pathlist)-i > 1000:
 			continue
 		elif len(pathlist)-i > 0:
-			openWord(file)
-			time.sleep(5)
+			if ".doc" in file:
+				openWord(file)
+			elif ".xls" in file:
+				openExcel(file)
+				# break
+			# time.sleep(5)
 		else:
 			break
 			
-	
+	try:
+		word.Quit()
+	except Exception as e:
+		pass
+	try:
+		excel.Quit()
+	except Exception as e:
+		pass
 	
 if __name__ == "__main__":
-	main()
-	pass
-	
+	try:
+		main()
+	except Exception as e:
+		print(e)
+	time.sleep(10)
 	
