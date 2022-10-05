fab95e2952bc30311beb1bab2e6313864ab3d057
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2022-10-05 14:04:42 +0800

    Update Replace.cmd

--- Text1/Replace.cmd
+++ Text1/Replace.cmd
@@ -3,22 +3,22 @@ REM echo 切换至 UTF8
 chcp 65001 >nul
 REM echo 开启延迟变量
 setlocal EnableDelayedExpansion
-REM echo 计算在磁盘根目录的次数
-set times=0 
+REM test=1 输出测试内容
+set test=0
 
 
-echo 当前目录
+if %test%==1 echo 当前目录
 echo %~dp0
 cd /d %~dp0
 goto FindPython
 
 
 :FindPython
-echo 寻找 Python 程序路径
+if %test%==1 echo 寻找 Python 程序路径
 dir /s/b | findstr "python.exe" > pythonpath.txt
 for /f "usebackq delims=," %%a in (pythonpath.txt) do (
 	set "PythonPath=%%~a"
-	echo !PythonPath!
+	if %test%==1 echo !PythonPath!
 	)
 del /q pythonpath.txt
 	
@@ -31,13 +31,13 @@ if "%cd%"=="%~d0\" (
 	)
 
 if "%PythonPath%"=="" (
-	echo 当前目录下没有 python.exe 
+	if %test%==1 echo 当前目录下没有 python.exe 
 	goto BackToParentPath
 	)^
 else (
-	echo.
-	echo 找到 python.exe
-	echo !PythonPath:"=!
+	if %test%==1 echo.
+	if %test%==1 echo 已经找到 python.exe
+	if %test%==1 echo !PythonPath:"=!
 	goto RunScript
 	)
 	
@@ -55,28 +55,33 @@ for /L %%a in (67, 1, 90) do (
 
 
 :BackToParentPath
-echo.
-echo 返回上级目录
+if %test%==1 echo.
+if %test%==1 echo 返回上级目录
 for %%a in ("%cd%") do set ParentPath=%%~dpa
-echo %ParentPath%
+if %test%==1 echo %ParentPath%
 cd %ParentPath%
 goto FindPython
 
 
 :RunScript
-echo.
-echo 寻找 Python 脚本路径
+if %test%==1 echo.
+if %test%==1 echo 寻找 Python 脚本路径
 set PythonFile="%~f0"
 set PythonFile=%PythonFile:.cmd=.py%
 title %PythonFile%
-echo %PythonFile%
-echo.
-echo 执行脚本
+if %test%==1 echo %PythonFile%
+
+if %test%==1 echo.
+if %test%==1 echo 执行脚本
 echo USE %PythonPath:"=%
 echo RUN %PythonFile:"=%
 echo.
-call "%PythonPath%"
-REM call "%PythonPath%" "%PythonFile%"
+if %test%==1 (
+	call "%PythonPath%"
+	)^
+else (
+	call "%PythonPath%" "%PythonFile%"
+	)
 goto End
 
 
