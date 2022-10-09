ab224d47afeb5418db3ee95f107f5489ffad2eac
DowneyRem <73387736+DowneyRem@users.noreply.github.com>
2022-10-09 08:15:41 +0800

    Update Replace.cmd
    将 pythonpath.txt 存放在 程序所在目录下

--- Text1/Replace.cmd
+++ Text1/Replace.cmd
@@ -1,6 +1,6 @@
 @echo off
 REM echo 切换至 UTF8
-chcp 65001 >nul
+chcp 65001 > nul
 REM echo 开启延迟变量
 setlocal EnableDelayedExpansion
 REM test=1 输出测试内容
@@ -10,17 +10,28 @@ set test=0
 if %test%==1 echo 当前目录
 echo %~dp0
 cd /d %~dp0
+set PythonPathTxt=%~dp0PythonPath.txt
+echo.
 goto FindPython
 
 
 :FindPython
 if %test%==1 echo 寻找 Python 程序路径
-dir /s/b | findstr "python.exe" > pythonpath.txt
-for /f "usebackq delims=," %%a in (pythonpath.txt) do (
+dir /s/b | findstr "python.exe" >> %PythonPathTxt%
+for /f "usebackq delims=," %%a in (%PythonPathTxt%) do (
 	set "PythonPath=%%~a"
 	if %test%==1 echo !PythonPath!
 	)
-del /q pythonpath.txt
+
+
+if %test%==1 (
+	echo. > Nul
+	REM del /q %PythonPathTxt%
+	)^
+else (
+	del /q %PythonPathTxt%
+	)
+	
 	
 REM 循环寻找Python程序，找到后运行脚本
 if "%cd%"=="%~d0\" (
@@ -29,7 +40,6 @@ if "%cd%"=="%~d0\" (
 		goto End
 		)
 	)
-
 if "%PythonPath%"=="" (
 	if %test%==1 echo 当前目录下没有 python.exe 
 	goto BackToParentPath
@@ -40,7 +50,7 @@ else (
 	if %test%==1 echo !PythonPath:"=!
 	goto RunScript
 	)
-
+	
 
 :BackToParentPath
 if %test%==1 echo.
@@ -57,7 +67,8 @@ if %test%==1 echo 寻找 Python 脚本路径
 set PythonFile="%~f0"
 set PythonFile=%PythonFile:.cmd=.py%
 title %PythonFile%
-if %test%==1 echo %PythonFile%
+if %test%==1 echo %PythonFile:"=%
+
 
 if %test%==1 echo.
 if %test%==1 echo 执行脚本
@@ -75,3 +86,4 @@ goto End
 
 :End
 pause & exit
+
